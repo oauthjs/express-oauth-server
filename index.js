660f586b706ce405e4d36d56d53d236b532a450a
Michael Salinger <mjsalinger@kinvey.com>
2016-06-01 11:29:04 -0400

    Changed handleResponse to use res.redirect instead of manually set location header

--- index.js
+++ index.js
@@ -119,10 +119,18 @@ ExpressOAuthServer.prototype.token = function(options) {
 /**
  * Handle response.
  */
-
 var handleResponse = function(req, res, response) {
-  res.set(response.headers);
-  res.status(response.status).send(response.body);
+
+  if (response.status === 302) {
+    var location = response.headers.location;
+    delete response.headers.location;
+    res.set(response.headers);
+    res.redirect(location);
+  }
+  else {
+    res.set(response.headers);
+    res.status(response.status).send(response.body);
+  }
 };
 
 /**
